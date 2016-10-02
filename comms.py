d3665fa50dc7b10415a047e342fa76dc3f9eed71
AngusP <a7cp3ar50n@gmail.com>
2016-10-02 12:58:27 +0100

    Kill comms states

--- comms.py
+++ comms.py
@@ -13,15 +13,6 @@ import time
 
 class Comms:
 
-    FORWARD = 0
-    BACKWARD = 1
-    LEFT = 2
-    RIGHT = 3
-    LEFT_FOLLOW = 4
-    RIGHT_FOLLOW = 5
-    BOREDOM_TURN_ON_SPOT = 6
-    BOREDOM_RUN_AWAY = 7
-
     port = serial.Serial()
 
     # Initialise the class, trying to open the Serial Port
