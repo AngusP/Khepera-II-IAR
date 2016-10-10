8cf9132f25dc7ccf192fb360edecacfc1bf4566b
AngusP <a7cp3ar50n@gmail.com>
2016-10-10 16:24:25 +0100

    Accepting hashes into statestream list in Redis

--- data.py
+++ data.py
@@ -7,26 +7,32 @@ import getopt
 
 class DataStore:
 
-    def __init__(self):
-        self.r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    def __init__(self, host='localhost', db=9, port=6379):
+        self.r = redis.StrictRedis(host=host, port=port, db=db)
         self.wt = whiptail.Whiptail()
         self.listname = "statestream"
 
     def push(self, this):
+        # We only accept a specific data type:
         if not isinstance(this, GenericState):
             raise TypeError
 
         todo = {"key1":1, "key2":2}
         
+        # For each part of the _python_ dict we
+        # create a Redis hashmap using the time as index
         for key, value in todo.iteritems():
             self.r.hset(this.time, key, value)
-            
+    
+        # We push a reference to this new hashmap onto 
+        # the statestream list
         self.r.lpush(self.listname, this.time)
+        # Decrememt reference counter
         del this
 
     def get(self, start, stop):
+        # Get references from list
         keys = self.r.lrange(self.listname, int(start), int(stop))
-        
         ret = dict()
 
         for key in keys:
