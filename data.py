e1b307e09e0882271fcc4fbc344b1ef8579b9144
AngusP <a7cp3ar50n@gmail.com>
2016-10-10 16:11:15 +0100

    Fleshing out redis/data class, added abstract class for states to be implemented by state later.

--- data.py
+++ data.py
@@ -1,8 +1,60 @@
+from __future__ import print_function
 import redis
-
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
+import whiptail
+from state import *
+import sys
+import getopt
 
 class DataStore:
 
     def __init__(self):
-        raise NotImplementedError
+        self.r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        self.wt = whiptail.Whiptail()
+        self.listname = "statestream"
+
+    def push(self, this):
+        if not isinstance(this, GenericState):
+            raise TypeError
+
+        todo = {"key1":1, "key2":2}
+        
+        for key, value in todo.iteritems():
+            print(key)
+            print(value)
+            
+        self.r.lpush(self.listname, todo)
+        del this
+
+    def get(self, start, stop):
+        return self.r.lrange(self.listname, int(start), int(stop))
+
+    def _purge(self):
+        if self.wt.confirm("Really destroy all data in Redis store?\n\nThis is not undoable!\n(run FLUSHDB)",
+                           default='no'):
+            self.r.flushdb()
+            print("!!! Flushed Redis Data Store !!!")
+        else:
+            print("Did not purge Redis Store")
+
+
+if __name__ == "__main__":
+
+    ds = DataStore()
+    s1 = GenericState()
+    
+    args = sys.argv[1:]
+
+    try:
+        optlist, args = getopt.getopt(args, 'p', ['purge','test'])
+    except getopt.GetoptError:
+        print("Invalid Option, correct usage:")
+        print("-p or --purge : Destroy all data held in Redis")
+        print("-t or --test  : Development tests")
+        sys.exit(2)
+
+    for opt, arg in optlist:
+        if opt in ('-p', '--purge'):
+            ds._purge()
+        elif opt in ('-t', '--test'):
+            ds.push(s1)
+
---
+++ state.py
@@ -0,0 +1,44 @@
+# ODOMETRY V1 (no calibration, dumb formulas based on encoders)
+# USES https://www.cs.princeton.edu/courses/archive/fall11/cos495/COS495-Lecture5-Odometry.pdf
+
+from math import *
+
+TICKS_PER_M = 12000 # encoder ticks per meter
+WHEEL_BASE_M = 0.01 # m
+MEASUREMENT_PERIOD_S = 0.02 # s
+
+
+# Class to define state to not have arrays... cause arrays are ugly
+class GenericState(object):
+  
+  def __init__(self):
+    time = None
+    # x location relative to initial placament at time of recording
+    x = None
+    # y location relative to initial placament at time of recording
+    y = None
+    # y location relative to initial placament at time of recording
+    theta = None
+    # encoder values at the time of recording
+    encoder_l = None
+    encoder_r = None
+
+  def delta_l_r(self, prev_l, prev_r):
+    raise NotImplementedError
+
+  def delta_s(self, prev_l, prev_r):
+    raise NotImplementedError
+
+  def delfa_theta(self, prev_l, prev_r):
+    raise NotImplementedError
+
+  def delta_x_y_angle(self, prev_l, prev_r, curr_theta):
+    raise NotImplementedError
+
+  def new_state(self, prev_state):
+    raise NotImplementedError
+
+# Subclass
+class OtherState(GenericState):
+  def __init__(self):
+    GenericState.__init__(self)
