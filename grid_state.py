f1ba60b5721dd4738fb501e66b55a66f345aeab0
Jevgenij Zubovskij <s1346981@sms.ed.ac.uk>
2016-11-03 01:25:33 +0000

    grid_state.py created online with Bitbucket

---
+++ grid_state.py
@@ -0,0 +1,28 @@
+#
+#     - - - -  I A R  - - - -
+#
+#  s1311631         Angus Pearson
+#  s1346981    Jevgenij Zubovskij
+#
+
+from AStar import Cell
+
+class Grid_State:
+
+    def __init__(self):
+           #variable for current path
+           self.active_path = []
+           
+           #variable for current path
+           self.current_cell = Cell(0,0, True)
+           
+           #variable for say alternative nests
+           self.alternative_paths = [[]]
+
+    #TODO add checks to see if this is empty
+
+
+    
+    
+    
+    
