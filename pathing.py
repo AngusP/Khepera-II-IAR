ad47d81e88a609b5593cb4558696c51580c4483e
Jevgenij Zubovskij <s1346981@sms.ed.ac.uk>
2016-11-03 00:55:56 +0000

    Basic pathing with vector approach to cells done

--- pathing.py
+++ pathing.py
@@ -8,6 +8,8 @@
 #
 
 from navigation_state import Navigation_State
+from AStar.py import AStar
+from AStar.py import Cell
 
 import constants
 import sys
@@ -15,28 +17,31 @@ import sys
 import time
 import math
 
+
+
+
+
 class Bug_Algorithm:
 
 
-	def lineMagnitude (self, x1, y1, x2, y2):
-	    lineMagnitude = math.sqrt(math.pow((x2 - x1), 2)+ math.pow((y2 - y1), 2))
-	    return lineMagnitude
-		
+	def __init__(self):
+		self.aStar = AStar()
+	
 	#vector magnitude calculator
 	def vector_magnitude(self, vector):
 		return math.sqrt( math.pow(vector[0],2) + math.pow(vector[1],2))
 		
 
 	#normalizes angle in degrees to -180 : 180 degrees
-        def normalize_angle(self, angle):
+    def normalize_angle(self, angle):
 		if angle < 0:
 		    angle = angle % -360
 		    if angle < -180:
-			angle = 360 + angle
+				angle = 360 + angle
 		else:
 		    angle = angle % 360
 		    if angle > 180:
-			angle = -(360 - angle)
+				angle = -(360 - angle)
 		return angle
 
 	#vector difference calculator
@@ -47,8 +52,7 @@ class Bug_Algorithm:
 
 		 
 	#get angle while ON the M-line
-	def get_angle_on_m(self, odo_state, bug_state):
-		direction = self.vector_diff( bug_state.m_line_start,  bug_state.m_line_end)
+	def get_angle_on_m(self, odo_state, direction):
 		direction_angle = math.atan2(direction[1] , direction[0])
 		#if no difference, well then we never left the spot 
 		vector_magnitude = self.vector_magnitude(direction)
@@ -58,55 +62,50 @@ class Bug_Algorithm:
 		direction_angle = math.degrees(direction_angle) - self.normalize_angle(math.degrees(odo_state.theta))
 		return self.normalize_angle(direction_angle)
 	
-    
+  
+	
     #TODO placeholder method, before any linearization etc.
-    def is_away_from_path(self, grid_state, grid):
-    def is_left_free(self, grid_state, grid):
-    def is_right_free(self, grid_state, grid):
+    def is_away_from_path(self, direction):
+			return vector_magnitude(self, direction) > constants.AWAY_FROM_PATH
+		
         
     
 	#return new state
-    def new_state(self, grid_state, grid):
-	                
+    def new_state(self, nav_state, odo_state, grid_state, grid):
+	     
+		current_cell 	 = grid_state.current_cell
+		next_cell    	 = grid_state.active_path[0]
+		direction = (next_cell.x - current_cell.x, next_cell.y - current_cell.y)
 	    
         speed_l = nav_state.speed_l 
 	    speed_r = nav_state.speed_r
+		
+		#turn aggressively
+		turn_less = -constants.CONST_SPEED 
+		turn_more = constants.CONST_SPEED 
 
-		#do not turn if not needed
+		#TODO make it say got to another nest or something if there is another one
 
-		turn_less = constants.CONST_SPEED 
-		turn_more = constants.CONST_SPEED
+		#recalculate path if for some reason strayed from it too far
+		if self.is_away_from_path(direction):
+			grid_state.active_path = aStar.replan(current_cell, (0,0) , grid) 
 
-		#recalculate M-line if such a need arises
-		if self.is_away_from_path(nav_state):
-			
-			
-			#turn aggressively
-			turn_less = -constants.CONST_SPEED 
-			turn_more = constants.CONST_SPEED 
 
-			angle_to_m = self.get_angle_on_m(odo_state, bug_state)
-
-			#OUR angle too small
-			if angle_to_m > constants.M_N_ANGLE:		
+		angle_to_m = self.get_angle_on_m(odo_state, direction)
+		#OUR angle too small
+		if angle_to_m > constants.M_N_ANGLE:		
 				
-					# check if there is a wall on the left
-					if self.is_left_free(grid_state, grid):
-						# if there is none, turn left
-						speed_r = turn_more
-						speed_l = turn_less	
+				#no need to check for obstacles as pathing takes cares of it for us
+				speed_r = turn_more
+				speed_l = turn_less	
 
-			#OUR angle too big
-			elif angle_to_m < -constants.M_N_ANGLE:
-
-					# check if there is a wall on the right
-					if self.is_right_free(grid_state, grid):
-						# if there is none, turn right
-						speed_r = turn_less
-						speed_l = turn_more
+		#OUR angle too big
+		elif angle_to_m < -constants.M_N_ANGLE:
+				#no need to check for obstacles as pathing takes cares of it for us
+				speed_r = turn_less
+				speed_l = turn_more
 
 		#send out the new speed controls			
 		nav_state.speed_l = speed_l
 		nav_state.speed_r = speed_r 
-		return nav_state
 		    
